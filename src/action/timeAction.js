a50618308cc7544083782462306048040c48fb29
Sumit Yadav <sumity159@gmail.com>
2021-09-19 23:51:01 +0530

    new changes

--- src/action/timeAction.js
+++ src/action/timeAction.js
@@ -1,6 +1,6 @@
 import { db } from "../firebase";
 import firebase from "firebase";
-import { SET_DAY_ID, SET_USERS_TIME_LIST, SET_USER_WORK_TILL_NOW, SET_USER_YESTERDAY_AND_TODAY_PUNCH } from "./types";
+import { ADD_PUNCH_TIME_DATA, SET_DAY_ID, SET_USERS_TIME_LIST, SET_USER_TIME_DATA, SET_USER_WORK_TILL_NOW, SET_USER_YESTERDAY_AND_TODAY_PUNCH } from "./types";
 
 import { toast } from "react-toastify";
 import { batch } from "react-redux";
@@ -10,68 +10,21 @@ export const GetUserTimingData = () => (dispatch, getState) => {
 	const { user } = getState().auth_store;
 	const userId = user.id;
 	const currentDay = new Date();
-
 	db.collection(userId)
-		.orderBy("timeStamp", "desc")
+		.orderBy("timeStamp", "asc")
 		.where("timeStamp", "<=", new Date(`${currentDay.getFullYear()}-${currentDay.getMonth() + 1}-31`))
 		.where("timeStamp", ">=", new Date(`${currentDay.getFullYear()}-${currentDay.getMonth() + 1}-01`))
-		.onSnapshot((querySnapshot) => {
-			const timeList = [];
-			let totalHour = null;
-			let hour = 0;
-			let min = 0;
-			let index = 0;
-
+		.get()
+		.then((querySnapshot) => {
 			querySnapshot.forEach((doc) => {
-				let day = "";
-				let hourDone = null;
-				let extraTime = null;
+				// let hourDone = null;
+				// let extraTime = null;
 
-				// saving day as 01-01-2021 format
-				if (doc.data().timeStamp) {
-					const { seconds, nanoseconds } = doc.data().timeStamp;
-					day = _getDateFromTimeStamp(seconds, nanoseconds);
-				}
-
-				// checking if entry time already punched
-				if (index === 0 && !!doc.data().entry) {
-					// checking if entry time was of today
-					const entryTime = new Date(doc.data().entry);
-					if (currentDay.getDate() === entryTime.getDate()) {
-						dispatch({ type: SET_DAY_ID, id: doc.id });
-					}
-				}
-				// calc hour done using entry and exit time
-				if (!!doc.data().entry && !!doc.data().exit) {
-					let ms = Math.abs(doc.data().entry - doc.data().exit);
-					hourDone = _msToTime(ms);
-					extraTime = _calculateExtraTime(hourDone);
-					totalHour += 9;
-					const splitHourDone = hourDone.split(":");
-					hour += parseInt(splitHourDone[0]);
-					min += parseInt(splitHourDone[1]);
-				}
-
-				// calculate current month data
-
-				const timeData = {
-					id: doc.id,
-					day,
-					entry: doc.data().entry || "",
-					exit: doc.data().exit || "",
-					hourDone: hourDone || "",
-					extraTime: extraTime || "",
-				};
-
-				timeList.push(timeData);
-				index++;
+				dispatch({ type: SET_USER_TIME_DATA, docId: doc.id, userData: doc.data() });
 			});
 
 			batch(() => {
-				dispatch({ type: SET_USERS_TIME_LIST, data: timeList });
-				dispatch({ type: SET_USER_WORK_TILL_NOW, data: { totalHour, hour, min } });
 				dispatch({ type: SET_USER_YESTERDAY_AND_TODAY_PUNCH });
-				// dispatch({ type: "SET_CURRENT_MONTH_DATA", userId });
 				FillLeftTimingsData()(dispatch, getState);
 			});
 		});
@@ -85,14 +38,11 @@ export const CreateEntryTime = (punchTime) => (dispatch, getState) => {
 
 	return db
 		.collection(userId)
-		.add(
-			{
-				entry: punchTime,
-				userId,
-				timeStamp: new Date(),
-			},
-			{ merge: true }
-		)
+		.add({
+			entry: punchTime,
+			userId,
+			timeStamp: new Date(),
+		})
 		.then((data) => {
 			dispatch({ type: SET_DAY_ID, id: data.id });
 			toast.success("successfully entry time added");
@@ -148,7 +98,22 @@ export const CreateEntryAndExitTimeBoth =
 				absentReason,
 				timeStamp,
 			})
-			.then(() => {
+			.then((data) => {
+				const docRef = db.collection(userId).doc(data.id);
+
+				docRef
+					.get()
+					.then((doc) => {
+						if (doc.exists) {
+							dispatch({ type: SET_USER_TIME_DATA, docId: doc.id, userData: doc.data() });
+						} else {
+							// doc.data() will be undefined in this case
+							console.log("No such document!");
+						}
+					})
+					.catch((error) => {
+						console.log("error getting the doc:", error);
+					});
 				toast.success("successfully time added");
 			})
 			.catch((error) => {
@@ -160,21 +125,27 @@ export const CreateEntryAndExitTimeBoth =
 /** @desc function for filling all left data by user like absent or sunday */
 export const FillLeftTimingsData = () => (dispatch, getState) => {
 	const { timeData } = getState().time_store;
-	const lastPunchTime = timeData[0];
 
-	const currentDate = new Date();
-	const lastPunchDay = new Date(lastPunchTime.entry || lastPunchTime.exit);
-	// fill when last punch
-	if (lastPunchDay.getDate() < currentDate.getDate()) {
-		for (let i = lastPunchDay.getDate() + 1; i < currentDate.getDate(); i++) {
-			let absentReason = "leave";
-			const date = new Date(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${i}`);
-			// absent reason sunday if day is 7
-			if (date.getDay() === 7) {
-				absentReason = "sunday";
+	if (timeData.length !== 0) {
+		const lastPunchTime = timeData[0];
+
+		const swappedLastPunchDay = `${lastPunchTime.day.substring(3, 5)}-${lastPunchTime.day.substring(0, 2)}-${lastPunchTime.day.substring(6)}`;
+
+		const currentDate = new Date();
+		const lastPunchDay = new Date(swappedLastPunchDay);
+
+		// fill when last punch
+		if (lastPunchDay.getDate() < currentDate.getDate()) {
+			for (let i = lastPunchDay.getDate() + 1; i < currentDate.getDate(); i++) {
+				let absentReason = "leave";
+				const date = new Date(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${i}`);
+				// absent reason sunday if day is 0
+				if (date.getDay() === 0) {
+					absentReason = "sunday";
+				}
+
+				CreateEntryAndExitTimeBoth("", "", absentReason, date)(dispatch, getState);
 			}
-
-			CreateEntryAndExitTimeBoth("", "", absentReason, date)(dispatch, getState);
 		}
 	}
 };
@@ -188,52 +159,4 @@ export const _getPunchDate = (punchTime) => {
 	const month = `${date.getMonth()}`.length === 1 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`;
 	const day = `${date.getDate()}`.length === 1 ? `0${date.getDate()}` : `${date.getDate()}`;
 	return day + month + year;
-};
-
-/** @desc helper function for converting ms to time */
-const _msToTime = (duration) => {
-	let seconds = Math.floor((duration / 1000) % 60);
-	let minutes = Math.floor((duration / (1000 * 60)) % 60);
-	let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-
-	hours = hours < 10 ? "0" + hours : hours;
-	minutes = minutes < 10 ? "0" + minutes : minutes;
-	seconds = seconds < 10 ? "0" + seconds : seconds;
-
-	return hours + ":" + minutes + ":" + seconds;
-};
-
-const _getDateFromTimeStamp = (sec, nanoSec) => {
-	const miliSeconds = (sec + nanoSec / 1000000000) * 1000;
-	const date = _getPunchDate(miliSeconds);
-	return `${date.substring(0, 2)}-${date.substring(2, 4)}-${date.substring(4)}`;
-};
-
-/** @desc helper function for calculating extra time */
-const _calculateExtraTime = (hourDone) => {
-	const splitHourDone = hourDone.split(":");
-	let splitHour = parseInt(splitHourDone[0]);
-	let splitMin = parseInt(splitHourDone[1]);
-	let splitSec = parseInt(splitHourDone[2]);
-	let extraHour = "";
-	let extraMin = (60 - splitMin).toString().length === 1 ? `0${60 - splitMin}` : 60 - splitMin;
-	let extraSec = (60 - splitSec).toString().length === 1 ? `0${60 - splitSec}` : 60 - splitSec;
-
-	// work done in only min
-	if (splitHour === 0) {
-		return `-08:${extraMin}:${extraSec}`;
-	}
-
-	// work done under 9 hour
-	if (Math.sign(splitHour - 9) === -1) {
-		extraHour = (splitHour - 9).toString().length === 1 ? `0${splitHour - 9}` : splitHour - 9;
-		return `${extraHour}:${extraMin}:${extraSec}`;
-	} else if (Math.sign(splitHour - 9) === +1) {
-		// work over 9 hour
-		extraHour = (splitHour - 9).toString().length === 1 ? `0${splitHour - 9}` : splitHour - 9;
-		return `${extraHour}:${extraMin}:${extraSec}`;
-	} else {
-		// extra minutes work done
-		return `+00:${extraMin}:${extraMin}`;
-	}
 };
--- src/reducers/timeReducer.js
+++ src/reducers/timeReducer.js
@@ -1,4 +1,11 @@
-import { SET_DAY_ID, SET_USERS_TIME_LIST, SET_USER_WORK_TILL_NOW, SET_USER_YESTERDAY_AND_TODAY_PUNCH } from "../action/types";
+import {
+	ADD_PUNCH_TIME_DATA,
+	SET_DAY_ID,
+	SET_USERS_TIME_LIST,
+	SET_USER_TIME_DATA,
+	SET_USER_WORK_TILL_NOW,
+	SET_USER_YESTERDAY_AND_TODAY_PUNCH,
+} from "../action/types";
 import { _getPunchDate } from "../action/timeAction";
 import { db } from "../firebase";
 
@@ -13,12 +20,59 @@ const INITIAL_STATE = {
 		entry: "",
 		exit: "",
 	},
-	totalHour: "",
+	totalHour: 0,
 	selectedDayId: "",
 };
 
 export const timeReducer = (state = INITIAL_STATE, action) => {
 	let newState = { ...state };
+
+	if (action.type === SET_USER_TIME_DATA) {
+		const currentDay = new Date();
+		let day = "";
+		let hourDone = null;
+		let extraTime = null;
+		let hour = 0;
+		let min = 0;
+
+		if (action.userData.timeStamp) {
+			const { seconds, nanoseconds } = action.userData.timeStamp;
+			// saving day as 01-01-2021 format
+			day = _getDateFromTimeStamp(seconds, nanoseconds);
+		}
+
+		// calculate hour done using entry and exit time
+		if (!!action.userData.entry && !!action.userData.exit) {
+			let ms = Math.abs(action.userData.entry - action.userData.exit);
+			hourDone = _msToTime(ms);
+			extraTime = _calculateExtraTime(hourDone);
+			newState.totalHour += 9;
+			const splitHourDone = hourDone.split(":");
+			hour += parseInt(splitHourDone[0]);
+			min += parseInt(splitHourDone[1]);
+		}
+
+		const timeInfo = {
+			id: action.docId,
+			day,
+			entry: action.userData.entry || "",
+			exit: action.userData.exit || "",
+			hourDone: hourDone || "",
+			extraTime: extraTime || "",
+		};
+
+		newState.timeData = [timeInfo, ...state.timeData];
+		// when entry time exit for current day
+		if (!!timeInfo.entry && !timeInfo.exit) {
+			const entryTime = new Date(timeInfo.entry);
+			if (currentDay.getDate() === entryTime.getDate()) {
+				newState.selectedDayId = timeInfo.id;
+			}
+		}
+
+		console.log("check time data", newState.timeData);
+		return newState;
+	}
 
 	if (action.type === SET_USERS_TIME_LIST) {
 		newState.timeData = action.data;
@@ -33,6 +87,7 @@ export const timeReducer = (state = INITIAL_STATE, action) => {
 			hour += min / 60;
 			min += min % 60;
 		}
+
 		hour = hour.toString().length === 1 ? `0${hour}` : `${hour}`;
 		min = min.toString().length === 1 ? `0${min}` : `${min} `;
 		newState.totalWorkDone = { hour, min };
@@ -62,6 +117,11 @@ export const timeReducer = (state = INITIAL_STATE, action) => {
 		return newState;
 	}
 
+	if (action.type === ADD_PUNCH_TIME_DATA) {
+		newState.timeData = [action.punchData, ...state.timeData];
+		return newState;
+	}
+
 	if (action.type === SET_DAY_ID) {
 		newState.selectedDayId = action.id;
 		return newState;
@@ -87,4 +147,52 @@ const convertTimeStampToTime = (entry, exit) => {
 	}
 
 	return obj;
+};
+
+const _getDateFromTimeStamp = (sec, nanoSec) => {
+	const miliSeconds = (sec + nanoSec / 1000000000) * 1000;
+	const date = _getPunchDate(miliSeconds);
+	return `${date.substring(0, 2)}-${date.substring(2, 4)}-${date.substring(4)}`;
+};
+
+/** @desc helper function for converting ms to time */
+const _msToTime = (duration) => {
+	let seconds = Math.floor((duration / 1000) % 60);
+	let minutes = Math.floor((duration / (1000 * 60)) % 60);
+	let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+	hours = hours < 10 ? "0" + hours : hours;
+	minutes = minutes < 10 ? "0" + minutes : minutes;
+	seconds = seconds < 10 ? "0" + seconds : seconds;
+
+	return hours + ":" + minutes + ":" + seconds;
+};
+
+/** @desc helper function for calculating extra time */
+const _calculateExtraTime = (hourDone) => {
+	const splitHourDone = hourDone.split(":");
+	let splitHour = parseInt(splitHourDone[0]);
+	let splitMin = parseInt(splitHourDone[1]);
+	let splitSec = parseInt(splitHourDone[2]);
+	let extraHour = "";
+	let extraMin = (60 - splitMin).toString().length === 1 ? `0${60 - splitMin}` : 60 - splitMin;
+	let extraSec = (60 - splitSec).toString().length === 1 ? `0${60 - splitSec}` : 60 - splitSec;
+
+	// work done in only min
+	if (splitHour === 0) {
+		return `-08:${extraMin}:${extraSec}`;
+	}
+
+	// work done under 9 hour
+	if (Math.sign(splitHour - 9) === -1) {
+		extraHour = (splitHour - 9).toString().length === 1 ? `0${splitHour - 9}` : splitHour - 9;
+		return `${extraHour}:${extraMin}:${extraSec}`;
+	} else if (Math.sign(splitHour - 9) === +1) {
+		// work over 9 hour
+		extraHour = (splitHour - 9).toString().length === 1 ? `0${splitHour - 9}` : splitHour - 9;
+		return `${extraHour}:${extraMin}:${extraSec}`;
+	} else {
+		// extra minutes work done
+		return `+00:${extraMin}:${extraMin}`;
+	}
 };
